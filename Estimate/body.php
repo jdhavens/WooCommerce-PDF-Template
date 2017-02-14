3e62774778c0b95e1fc584f8f9e61c7392486609
jdhavens <jdhavens@gmail.com>
2017-02-14 07:05:16 -0500

    Removed Shipping Address

--- Estimate/body.php
+++ Estimate/body.php
@@ -14,14 +14,9 @@ echo $this->outlining_columns_html( count( $this->order->get_taxes() ) );
 				printf( __( 'Phone: %s', 'woocommerce-pdf-invoices' ), $this->order->billing_phone );
 			endif; ?>
 		</td>
-		<?php
-		$formatted_shipping_address = $this->order->get_formatted_shipping_address();
-		if ( ! empty( $this->template_options['bewpi_show_ship_to'] ) && ! empty( $formatted_shipping_address ) && ! $this->has_only_virtual_products() ) { ?>
 			<td class="address small-font" width="50%">
-				<b><?php _e( 'Ship to', 'woocommerce-pdf-invoices' ); ?></b><br/>
-				<?php echo $formatted_shipping_address; ?>
 			</td>
-		<?php } ?>
+
 	</tr>
 	</tbody>
 </table>
